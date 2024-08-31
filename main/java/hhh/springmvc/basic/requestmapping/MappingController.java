84f87856285513f807c080cca97750b174ae7f39
mel <melaminn38@gmail.com>
2024-08-31 22:32:21 +0900

    exm4

---
+++ main/java/hhh/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,36 @@
+package hhh.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/hello-basic")
+    public String helloBasic(){
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data){
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long
+            orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+}
